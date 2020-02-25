a9fd19e5ad4e745266247c53a1056185c3fe3c38
Andrew Sokolenko <a5okowork@gmail.com>
2020-02-25 15:12:44 +0200

    Firebase. Преднастройка и настройка отправки заметок.

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -3,19 +3,38 @@ import { Alert } from "react-native";
 
 import { TodoContext } from "./todoContext";
 import { todoReducer } from "./todoReducer";
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "./types";
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  UPDATE_TODO,
+  SHOW_LOADER,
+  SHOW_ERROR
+} from "./types";
 import { ScreenContext } from "../screen/screenContext";
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: "1", title: "Пахать очень плодотворно" }]
+    todos: [],
+    loading: false,
+    error: null
   };
 
   const { changeScreen } = useContext(ScreenContext);
-
   const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  const addTodo = title => dispatch({ type: ADD_TODO, title });
+  const addTodo = async title => {
+    const response = await fetch(
+      "https://rn-todo-application.firebaseio.com/todos.json",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title }) // JSON - глобальный метод, который применяется для того, что бы привести все к строке.
+      }
+    );
+
+    const data = await response.json(); //
+    dispatch({ type: ADD_TODO, title, id: data.name });
+  };
 
   const removeTodo = id => {
     const todo = state.todos.find(t => t.id === id);
@@ -42,6 +61,14 @@ export const TodoState = ({ children }) => {
   };
 
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+
+  const showLoader = () => dispatch({ type: SHOW_LOADER });
+
+  const hideLoader = () => dispatch({ type: HIDE_LOADER });
+
+  const showError = error => dispatch({ type: SHOW_ERROR, error });
+
+  const clearError = () => dispatch({ type: CLEAR_ERROR });
 
   return (
     <TodoContext.Provider
--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,15 +1,18 @@
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "./types";
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  UPDATE_TODO,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  CLEAR_ERROR,
+  FETCH_TODOS,
+  SHOW_ERROR
+} from "./types";
 
 const handlers = {
-  [ADD_TODO]: (state, { title }) => ({
+  [ADD_TODO]: (state, { title, id }) => ({
     ...state,
-    todos: [
-      ...state.todos,
-      {
-        id: Date.now().toString(),
-        title
-      }
-    ]
+    todos: [...state.todos, { id, title }]
   }),
   [REMOVE_TODO]: (state, { id }) => ({
     ...state,
@@ -24,10 +27,16 @@ const handlers = {
       return todo;
     })
   }),
+  [SHOW_LOADER]: state => ({ ...state, loading: true }),
+  [HIDE_LOADER]: state => ({ ...state, loading: false }),
+  [CLEAR_ERROR]: state => ({ ...state, error: null }),
+  [SHOW_ERROR]: (state, { error }) => ({ ...state, error }),
+  [FETCH_TODOS]: (state, { todos }) => ({ ...state, todos }),
+
   DEFAULT: state => state
 };
 
 export const todoReducer = (state, action) => {
- const handler = handlers [action.type] || handlers.DEFAULT
- return handler(state, action)
+  const handler = handlers[action.type] || handlers.DEFAULT;
+  return handler(state, action);
 };

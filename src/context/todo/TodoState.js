290a70f5a311be39ca046ab8517d1de746f0a08f
Andrew Sokolenko <a5okowork@gmail.com>
2020-02-27 14:09:40 +0200

    Firebase. Настройка удаления элементов с сервера.

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -53,8 +53,15 @@ export const TodoState = ({ children }) => {
         {
           text: "Delete",
           style: "destructive",
-          onPress: () => {
+          onPress: async () => {
             changeScreen(null);
+            await fetch(
+              `https://rn-todo-application.firebaseio.com/todos/${id}.json`,
+              {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" }
+              }
+            );
             dispatch({ type: REMOVE_TODO, id });
           }
         }
